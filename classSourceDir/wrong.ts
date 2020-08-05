8b87dca5c1cd2e09b627fd078ef142527acbcec4
OlivierSofia <olivier.sofia@qruz.com>
2020-08-05 09:41:52 +0200

    adding ts file

---
+++ classSourceDir/wrong.ts
@@ -0,0 +1,24 @@
+class Person2 qdsfqsdf{
+    private _id: string;
+    protected name: string;
+
+    constructor() {
+        this._id = uuid.v4();
+    }
+
+    public getName(): string {
+        return this.name;
+    }
+
+    public setName(name: string): void {
+        this.name = name;
+    }
+
+    public getId(): string {
+        return this._id;
+    }
+
+    public static build(): Person {
+        return new Person();
+    }
+}
