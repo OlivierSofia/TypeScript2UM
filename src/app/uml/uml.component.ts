55f3f8514f5aa487fa309dbc178fa7f88dd8890b
OlivierSofia <olivier.sofia@qruz.com>
2020-08-05 10:06:20 +0200

    removing uml lib ref

--- src/app/uml/uml.component.ts
+++ src/app/uml/uml.component.ts
@@ -1,6 +1,4 @@
 import { Component, ViewEncapsulation, ViewChild } from "@angular/core";
-import { DiagramComponent } from '@syncfusion/ej2-angular-diagrams';
-import { NodeModel, UmlClassifierShapeModel } from '@syncfusion/ej2-diagrams';
 
 @Component({
   selector: 'app-uml',
@@ -11,30 +9,5 @@ import { NodeModel, UmlClassifierShapeModel } from '@syncfusion/ej2-diagrams';
 export class UmlComponent  {
 
 
-  @ViewChild("diagram")
-  public diagram: DiagramComponent;
-
-  constructor() { }
-
-  public nodes: NodeModel[] = [
- {
-  id: "Patient",
-  //Position of the node
-  offsetX: 200,
-  offsetY: 200,
-  shape: {
-    type: "UmlClassifier",
-    //Define class object
-    class: {
-      name: "Patient",
-      //Define class attributes
-      attributes: [{ name: "accepted", type: "Date" }],
-      //Define class methods
-      methods: [{ name: "getHistory", type: "getHistory" }]
-    },
-    classifier: "Class"
-  } as UmlClassifierShapeModel
- }
-];
 
 }
